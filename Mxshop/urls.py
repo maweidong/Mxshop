12a2504d4966727dcf8ff85c4746c7c55310ec06
maweidong <5112717+maweidong@user.noreply.gitee.com>
2020-05-23 21:58:22 +0800

    添加goodsurl

--- Mxshop/urls.py
+++ Mxshop/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 
+from goods.views_base import GoodsListView
 
 urlpatterns = [
+    url(r'^goods/', GoodsListView.get, name="goods"),
+
 ]
